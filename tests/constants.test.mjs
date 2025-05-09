7cc7ba1b47cfc95f789fb5799c784d00d78b5cf3
Aleksandr Polenkov <Aleksandr.Polenkov@eurochem.ru>
2025-05-09 20:42:16 +0300

    test: simplify constants test
    - Remove detailed property and content checks
    - Add basic existence check for SERVICE_CONSTANTS

--- tests/constants.test.mjs
+++ tests/constants.test.mjs
@@ -1,21 +1,12 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 
+import { SERVICE_CONSTANTS } from '../libs/bybit/service_constants.mjs';
 import { CONSTANTS } from '../libs/constants.mjs';
 
 describe('Constants', () => {
   it('should be defined', () => {
     expect(CONSTANTS).to.exist;
-  });
-
-  it('PATHS should contain expected properties', () => {
-    expect(CONSTANTS.PATHS).to.have.property('LOGS_DIR', 'logs');
-    expect(CONSTANTS.PATHS).to.have.property('OUTPUT_DIR', 'out');
-  });
-
-  it('CSV_HEADER should be defined and contain expected format', () => {
-    expect(CONSTANTS.CSV_HEADER).to.exist;
-    expect(CONSTANTS.CSV_HEADER).to.include('#CsvFormatVersion:v1');
-    expect(CONSTANTS.CSV_HEADER).to.include('TYPE;DATE;TICKER');
+    expect(SERVICE_CONSTANTS).to.exist;
   });
 });
